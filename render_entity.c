25f4171f56b1c63644596a9a0d073eee167e7c9a
pinchyCZN <mail2@crazynation.org>
2014-01-28 07:08:58 -0600

    updates

--- render_entity.c
+++ render_entity.c
@@ -16,11 +16,11 @@ int test()
 	glPushMatrix();
 	glRotatef(theta, 0.0f, 0.0f, 1.0f);
 
-//	glBegin(GL_TRIANGLES);
-//	glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
-//	glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
-//	glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
-//	glEnd();
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
+	glEnd();
 
 
 	glPopMatrix();
